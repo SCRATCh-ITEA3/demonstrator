65507c3a3b4dddb6289e8347130232fb033d427b
xabiervalencia <50487612+xabiervalencia@users.noreply.github.com>
2020-03-17 16:29:45 +0100

    added previous printf

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -13,6 +13,16 @@ int main(void)
     printf("%s", str);
     getHelloWorldMessage(2, str);
     printf("%s", str);
+    int v = 2;
+    
+    printf("Hello SCRATCh world!\n");
+    printf("Hello Hamburg!\n");
+    printf("Hello Rotterdam!\n");
+    printf("Hello Bremen!\n");
+    printf("Hello Online!\n");
+    printf("Hello Team!\n");
+    printf("1 + 1 = %d\n", v);
+    
     return 0;
 }
 #endif // !TEST
