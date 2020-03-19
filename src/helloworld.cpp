6c6d74742ba9dfc89fa114de12c8700c78e91ef5
Till Witt <tlwt@users.noreply.github.com>
2020-03-19 11:25:15 +0100

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -4,11 +4,11 @@ int main(void)
 {
     int v = 2;
     printf("Hello SCRATCh world!\n");
-    printf("Hello Hamburg!\n");
-    printf("Hello Rotterdam!\n");
-    printf("Hello Bremen!\n");
     printf("Hello Bucharest!\n");
+    printf("Hello Bremen!\n");
+    printf("Hello Hamburg!\n");
     printf("Hello Online!\n");
+    printf("Hello Rotterdam!\n");
     printf("Hello Team!\n");
     printf("1 + 1 = %d\n", v);
     return 0;
