ea253b6259fb71ec70fc874def9ab54f04002d16
Krishna <ctrl-shift-esc@users.noreply.github.com>
2020-03-22 00:04:36 +0100

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -10,6 +10,6 @@ int main(void)
     printf("Hello Rotterdam!\n");
     printf("Hello Team!\n");
     printf("Hello Home Office\n");
-    printf("Little more git status check\n");
+    printf("status change check\n");
     return 0;
 }
