8e32ffd9e3714b77917f7ced445a2dc6a288203c
Krishna <ctrl-shift-esc@users.noreply.github.com>
2020-03-24 08:32:42 +0100

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -10,6 +10,6 @@ int main(void)
     printf("Hello Rotterdam!\n");
     printf("Hello Team!\n");
     printf("Hello Home Office\n");
-    printf("Loop test\n");
+    printf("Hot code check\n");
     return 0;
 }
