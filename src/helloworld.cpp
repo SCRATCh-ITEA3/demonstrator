8417ad16c672685fb203d0a100e6e20dd72bb894
Krishna <ctrl-shift-esc@users.noreply.github.com>
2020-03-24 10:38:06 +0100

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -10,6 +10,6 @@ int main(void)
     printf("Hello Rotterdam!\n");
     printf("Hello Team!\n");
     printf("Hello Home Office\n");
-    printf("Display in i.MX 6\n");
+    printf("Hello on i.MX 6\n");
     return 0;
 }
