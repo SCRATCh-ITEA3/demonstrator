37bd7c864825604806b9ba13048c8d15b60f2021
Till Witt <tlwt@users.noreply.github.com>
2020-03-30 15:50:48 +0200

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -3,13 +3,6 @@
 int main(void)
 {
     printf("Hello SCRATCh world!\n");
-    printf("Hello ICC and CIP Team!\n");
-    printf("Hello Everybody!\n");
-    printf("Hello Hamburg!\n");
-    printf("Hello Online!\n");
-    printf("Hello Rotterdam!\n");
-    printf("Hello Team!\n");
-    printf("Hello Home Office\n");
-    printf("Docker demo hello!\n");
+    printf("Hello Everybody, Hamburg, Rotterdam!\n");
     return 0;
 }
