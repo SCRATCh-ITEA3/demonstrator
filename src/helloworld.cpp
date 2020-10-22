fa5280bf7f90ad473222c75139461465d822f1ad
Till Witt <tlwt@users.noreply.github.com>
2020-10-22 16:12:32 +0200

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -3,10 +3,5 @@
 int main(void)
 {
     printf("Hello SCRATCh world!\n");
-    printf("For our demonstrator!\n");
-    printf("Hello Everybody, Hamburg, Rotterdam!\n");
-    printf("Welcome to the 2020-10-28 review!\n");
-    printf("Lets go!\n");    
-    printf("Live demo!\n");    
     return 0;
 }
