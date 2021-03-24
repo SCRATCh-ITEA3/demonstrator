568c3a80f89ea52d9f9a9cf91b71a1c12be6279e
Théo Rigas (windBlaze) <trigas@nviso.be>
2021-03-24 22:21:15 +0100

    Code style fixes

--- ioxy/broker.js
+++ ioxy/broker.js
@@ -1,16 +1,16 @@
-const aedes = require("aedes")()
-const server = require("net").createServer(aedes.handle)
-const port = 1884
+const aedes = require("aedes")();
+const server = require("net").createServer(aedes.handle);
+const port = 1884;
 
 server.listen(port, function () {
-    console.log("\n==== Remote Door Controller ====\n") 
-    console.log("[+] MQTT broker started and listening on port ", port)
-})
+    console.log("\n==== Remote Door Controller ====\n");
+    console.log("[+] MQTT broker started and listening on port ", port);
+});
 
 aedes.on("publish", async function (packet, client) {
     if (client != null) {
         // console.log(client.id)
-        console.log("[!] Command received! Opening \x1b[31mDoor " + packet.topic.substring(18,19) + "\x1b[0m")
+        console.log("[!] Command received! Opening \x1b[31mDoor " + packet.topic.substring(18,19) + "\x1b[0m");
         // console.log('[!] Client \x1b[31m' + client.id + '\x1b[0m has published', packet.payload.toString(), 'Topic: ', packet.topic)
     }
-})
+});
